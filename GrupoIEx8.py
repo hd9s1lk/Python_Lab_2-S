e424d4efc487998df69699f267043e51a1d473e1
Henrique <hd9business@gmail.com>
2023-04-28 15:16:48 +0100

    GrupoIEx8

---
+++ GrupoIEx8.py
@@ -0,0 +1,8 @@
+# Elabore um programa python que de entre três números fornecidos pelo utilizador,
+#permita encontrar o maior deles.
+
+num1 = float(input("Insira um primeiro numero: \n"))
+num2 = float(input("Insira um segundo numero: \n"))
+num3 = float(input("Insira um terceiro numero: \n"))
+
+print(max(num1,num2,num3))
